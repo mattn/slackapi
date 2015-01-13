d8c8f4fd225c2c1a8bfc07a3a2d0d46be154fb3a
Pierre-Alexandre St-Jean <pa@stjean.me>
2015-01-13 12:37:42 -0500

    reformatting of doc code example

--- slackbot.go
+++ slackbot.go
@@ -2,30 +2,30 @@
 //
 // Here is an example integration:
 //
-//     package main
+//   package main
 //
-//     import (
-//     	"fmt"
-//     	"github.com/pastjean/slackbot"
-//     	"log"
-//     	"os"
-//     )
+//   import (
+//   	"fmt"
+//   	"github.com/pastjean/slackbot"
+//   	"log"
+//   	"os"
+//   )
 //
-//     func main() {
-//     	s := slackbot.NewSlackBot()
+//   func main() {
+//     s := slackbot.NewSlackBot()
 //
-//     	s.OnMessageEvents(func(evt slackbot.MessageEvent) {
-//     		fmt.Printf("%v\n", evt)
-//     	})
+//     s.OnMessageEvents(func(evt slackbot.MessageEvent) {
+//       fmt.Printf("%v\n", evt)
+//     })
 //
-//     	token := os.Getenv("SLACK_TOKEN")
-//     	if token == "" {
-// 	    	log.Fatal("SLACK_TOKEN environment variable should be set")
-// 	    }
+//     token := os.Getenv("SLACK_TOKEN")
+//     if token == "" {
+// 	   	 log.Fatal("SLACK_TOKEN environment variable should be set")
+// 	   }
 //
-//     	s.SetToken(token)
-//     	log.Fatal(s.Start())
-//     }
+//     s.SetToken(token)
+//     log.Fatal(s.Start())
+//   }
 package slackbot
 
 import (
